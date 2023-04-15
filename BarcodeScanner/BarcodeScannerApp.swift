1144658f790f3c4c21688b25e77a6567e5a6ef8a
Alex Desjardins <ardesjardins91@gmail.com>
2023-04-15 12:14:13 -0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Alex Desjardins on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
